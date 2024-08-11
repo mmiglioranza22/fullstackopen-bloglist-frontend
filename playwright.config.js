7cc231b6af167c147f833de769e9224a8088e04e
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-08-11 16:40:33 -0300

    #skip

--- playwright.config.js
+++ playwright.config.js
@@ -11,8 +11,7 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  timeout: 30000,
-  globalTimeout: 60000,
+  timeout: 3000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   // fullyParallel: true,
@@ -29,7 +28,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:5173",
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -74,9 +73,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev:start:test",
+    // url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
